f0e903f5fd35f63fccd13394e1ccd892db950175
Louis Abery <60328026+aybery@users.noreply.github.com>
2022-03-16 22:18:16 +0000

    html and css improvements

--- main.py
+++ main.py
@@ -5,14 +5,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-  return render_template("home.html")
+    return render_template("home.html")
+
+
+@app.route("/upload/")
+def upload():
+    return render_template("upload.html")
 
 
 @app.route("/about/")
 def about():
-  return render_template("about.html")
+    return render_template("about.html")
 
 
 if __name__ == "__main__":
-  app.run(debug=True)
-
+    app.run(debug=True)
