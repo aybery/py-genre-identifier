09726b214ec3390bd82b809fe121464fc48d6204
Louis Abery <60328026+aybery@users.noreply.github.com>
2022-04-14 21:11:26 +0100

    Added ability to upload files

--- main.py
+++ main.py
@@ -1,16 +1,49 @@
-from flask import Flask, render_template, url_for
+import os
+
+from flask import Flask, render_template, url_for, request
+from werkzeug.utils import secure_filename
+
+ALLOWED_EXTENSIONS = {'wav', 'mp3'} # only .wav and .mp3 files can be uploaded
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'Audio'
+app.config['MAX_CONTENT_LENGTH'] = 800 * 1000 * 1000 # 800MB max size
 
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route("/")
 def home():
     return render_template("home.html")
 
 
-@app.route("/upload/")
+@app.route('/upload/', methods=['GET', 'POST'])
 def upload():
-    return render_template("upload.html")
+    message = ""
+    if request.method == 'POST':
+        f = request.files['file']
+        print(f.filename, "this is the file name :)")
+
+        # checks if the user actually uploaded anything
+        if f.filename == "":
+            message = "no data??"
+        else:
+            # Checks if there is a folder called "Audio" to put any files that are uploaded into
+            path = os.getcwd() + "\Audio"
+            if os.path.isdir(path):
+                pass
+            else:
+                os.mkdir("Audio")
+
+            # gets the complete path of the file and tries to save it
+            path = os.path.join(app.config['UPLOAD_FOLDER'], f.filename)
+            if os.path.isfile(path):
+                message = "error saving file, it already exists"
+            else:
+                f.save(path)
+                message = "file uploaded successfully"
+    return render_template('upload.html', message=message)
 
 
 @app.route("/about/")
@@ -20,3 +53,4 @@ def about():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
